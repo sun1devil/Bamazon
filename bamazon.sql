042a832d631a42f141ed5c7252b7593fa11813ad
sun1devil <codejmc11@gmail.com>
2018-05-24 18:26:54 -0700

    create user prompts and write some Psuedocode for page SQL updates and page logic

--- bamazon.sql
+++ bamazon.sql
@@ -14,10 +14,10 @@ CREATE TABLE products(
 );
 
 INSERT INTO products (item_id, productName, departmentName,  price, inventory) values (1, 'kitten', 'feline',  25.22, 100);
-INSERT INTO products (item_id, productName, departmentName,  price, inventory) values (1, 'cat', 'feline',  12.02, 50);
-INSERT INTO products (item_id, productName, departmentName,  price, inventory) values (1, 'puppy', 'canine',  20, 75);
-INSERT INTO products (item_id, productName, departmentName,  price, inventory) values (1, 'dog', 'canine',  5.50, 10);
-INSERT INTO products (item_id, productName, departmentName,  price, inventory) values (1, 'chick', 'avion',  100, 15);
-INSERT INTO products (item_id, productName, departmentName,  price, inventory) values (1, 'chicken', 'avion',  5000, 1);
+INSERT INTO products (item_id, productName, departmentName,  price, inventory) values (2, 'cat', 'feline',  12.02, 50);
+INSERT INTO products (item_id, productName, departmentName,  price, inventory) values (10, 'puppy', 'canine',  20, 75);
+INSERT INTO products (item_id, productName, departmentName,  price, inventory) values (11, 'dog', 'canine',  5.50, 10);
+INSERT INTO products (item_id, productName, departmentName,  price, inventory) values (20, 'chick', 'avian',  100, 15);
+INSERT INTO products (item_id, productName, departmentName,  price, inventory) values (21, 'chicken', 'avian',  5000, 1);tem
 
 SELECT * FROM products
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -11,13 +11,106 @@ var connection = mysql.createConnection({
 
   // Your password
   password: "#Devils1",
-  database: "playlist_DB"
+  database: "bamazon_DB"
 });
 
 connection.connect(function(err) {
   if (err) throw err;
-  console.log("connected as id " + connection.threadId);
-  afterConnection();
+//   console.log("connected as id " + connection.threadId);
+  
 });
 
-SELECT * FROM products;
+function readBamazon() {
+    console.log("Selecting all products...\n");
+    connection.query("SELECT * FROM products", function(err, res) {
+      if (err) throw err;
+      // Log all results of the SELECT statement
+      console.log(res);
+      connection.end();
+    });
+  }
+
+  inquirer.prompt(
+    {
+        type: "input",
+        name: "userItem",
+        message: "Please enter the ID of the item you would like to buy?",
+
+        // Capture response from name
+    }).then(function (response) {
+        console.log (response)});
+
+
+        inquirer.prompt(
+            {
+                type: "input",
+                name: "userQuantity",
+                message: "How many would you like to purchase?",
+        
+                // Capture response from name
+            }).then(function (response) {
+                console.log (response)});
+
+       
+
+
+
+// ******************************************************************************
+
+//   function createProduct() {
+//     console.log("Inserting a product...\n");
+//     var query = connection.query(
+// "INSERT INTO products SET ?",
+//     {
+//       item_id: 1,
+//       productName: 'lizard',
+//       departmentName: 'reptile',
+//       price: 6.23,
+//       inventory: 56
+
+//     },
+//     function(err, res) {
+//         console.log(res.affectedRows + " product added!\n");
+//         // Call updateProduct AFTER the INSERT completes
+//         updateProduct();
+//     }
+// );
+
+
+// ******************************************************************************
+
+// function updateProduct() {
+//     console.log("Updating all lizard inventory...\n");
+//     var query = connection.query(
+//       "UPDATE products SET ? WHERE ?",
+//       [
+//         {
+//           quantity: 100
+//         },
+//         {
+//           productName: 'lizard'
+//         }
+//       ],
+//       function(err, res) {
+//         console.log(res.affectedRows + " products updated!\n");
+//         // Call deleteProduct AFTER the UPDATE completes
+//         deleteProduct();
+//       }
+//     );
+
+// ******************************************************************************
+
+// function deleteProduct() {
+//     console.log("Deleting all canines...\n");
+//     connection.query(
+//       "DELETE FROM products WHERE ?",
+//       {
+//         departmentName: "canine"
+//       },
+//       function(err, res) {
+//         console.log(res.affectedRows + " products deleted!\n");
+//         // Call readProducts AFTER the DELETE completes
+//         readProducts();
+//       }
+//     );
+//   }
